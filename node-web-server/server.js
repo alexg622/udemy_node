ddbe1e4a868e9d0842086f5c352e38830037674d
alexg622 <alex.gm62288@gmail.com>
2018-09-03 20:05:15 -0700

    make better"

---
+++ node-web-server/server.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// })
+
+hbs.registerPartials(__dirname + "/views/partials")
+app.use(express.static(__dirname + "/public"))
+
+app.use((req, res, next) => {
+  let now = new Date().toString()
+  let log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log("Unable to append server.log");
+    }
+  })
+  next()
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper("screamIt", (text) => {
+  return text.toUpperCase()
+})
+
+app.get(('/'), (req, res) => {
+  res.send({
+    name: "Alex",
+    likes: [
+      "biking",
+      "cities"
+    ]
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "About Page",
+  })
+})
+
+app.get('/home', (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to my website"
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: "Unable to handle request"
+  })
+})
+app.listen(3000, () => console.log("server is up on port 3000"))
